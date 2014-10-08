7387d48eb0a84ea19aa4a4601b08cef19e69e541
Maria Shaffer <ria_torr@yahoo.com>
2014-10-07 21:37:41 -0400

    New webpage Sinatra

---
+++ README.md
@@ -0,0 +1,5 @@
+# Playing With Sinatra
+
+I worked off of this tutorial to create another webpage for my 100 Webpages in 100 Days project. I learned about how to use Ruby Sinatra.
+
+http://code.tutsplus.com/tutorials/singing-with-sinatra--net-18965
---
+++ basics.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  "You said '#{params[:message]}'"
+end
+
+get '/secret' do
+  erb :secret
+end
+
+post '/secret' do
+  params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+  params[:secret].reverse
+end
+
+not_found do
+  halt 404, 'page not found'
+end
